fa967679aa209617bc334669cd65eba8263cfed9
ContemporaryNietzsche <jose637007@gmail.com>
2024-08-11 15:31:04 -0400

    changed view to show the board better(still has visual bugs)

--- src/main/java/edu/rpi/legup/puzzle/minesweeper/MinesweeperView.java
+++ src/main/java/edu/rpi/legup/puzzle/minesweeper/MinesweeperView.java
@@ -14,42 +14,31 @@ import org.jetbrains.annotations.NotNull;
 public class MinesweeperView extends GridBoardView {
 
     private static final Logger LOGGER = LogManager.getLogger(MinesweeperView.class.getName());
-    public static final Image BOMB_IMAGE;
-
-    public static final Image EMPTY_IMAGE;
+    private static final Image BOMB_IMAGE;
+    private static final Image EMPTY_IMAGE;
+    private static final Color BACKGROUND_COLOR = new Color(230, 230, 250); // Lavender background
+    private static final Font CELL_FONT = new Font("Arial", Font.BOLD, 20);
 
     static {
         Image tempBombImage = null;
+        Image tempEmptyImage = null;
         try {
-            tempBombImage =
-                    ImageIO.read(
-                            Objects.requireNonNull(
-                                    ClassLoader.getSystemClassLoader()
-                                            .getResource(
-                                                    "edu/rpi/legup/images/minesweeper/tiles/Bomb.png")));
+            tempBombImage = ImageIO.read(Objects.requireNonNull(
+                    ClassLoader.getSystemClassLoader().getResource(
+                            "edu/rpi/legup/images/minesweeper/tiles/Bomb.png")));
+            tempEmptyImage = ImageIO.read(Objects.requireNonNull(
+                    ClassLoader.getSystemClassLoader().getResource(
+                            "edu/rpi/legup/images/minesweeper/tiles/Empty.png")));
         } catch (IOException e) {
             LOGGER.error("Failed to open Minesweeper images");
         }
         BOMB_IMAGE = tempBombImage;
-    }
-
-    static {
-        Image tempEmptyImage = null;
-        try {
-            tempEmptyImage =
-                    ImageIO.read(
-                            Objects.requireNonNull(
-                                    ClassLoader.getSystemClassLoader()
-                                            .getResource(
-                                                    "edu/rpi/legup/images/minesweeper/tiles/Empty.png")));
-        } catch (IOException e) {
-            LOGGER.error("Failed to open Minesweeper images");
-        }
         EMPTY_IMAGE = tempEmptyImage;
     }
 
     public MinesweeperView(@NotNull MinesweeperBoard board) {
         super(new BoardController(), new MinesweeperController(), board.getDimension());
+        setBackground(BACKGROUND_COLOR);
 
         for (PuzzleElement<?> puzzleElement : board.getPuzzleElements()) {
             final MinesweeperCell cell = (MinesweeperCell) puzzleElement;
@@ -57,9 +46,20 @@ public class MinesweeperView extends GridBoardView {
             final MinesweeperElementView elementView = new MinesweeperElementView(cell);
             elementView.setIndex(cell.getIndex());
             elementView.setSize(elementSize);
-            elementView.setLocation(
-                    new Point(loc.x * elementSize.width, loc.y * elementSize.height));
+            elementView.setFont(CELL_FONT); // Set custom font
+            elementView.setLocation(new Point(loc.x * elementSize.width, loc.y * elementSize.height));
+            elementView.setOpaque(true);
+            elementView.setBackground(new Color(245, 245, 220)); // Beige color for cells
+            elementView.setBorder(BorderFactory.createLineBorder(Color.DARK_GRAY)); // Cell border
             elementViews.add(elementView);
         }
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        g.setColor(Color.DARK_GRAY);
+        g.setFont(CELL_FONT);
+        g.drawString("Minesweeper", getWidth() / 2 - 50, 30); // Title at the top
     }
 }
